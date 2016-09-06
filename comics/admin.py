8044aad5a95c786e6d3aa327a0e46a5564fa739a
agentofchaos <rahulvsenna@gmail.com>
2016-09-06 09:54:47 +0530

    Stacked Inlined chapters to Series

--- comics/admin.py
+++ comics/admin.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from .models import Series, Chapters, Ratings
 
 
-class SeriesModelAdmin(admin.ModelAdmin):
+class ChaptersInline(admin.StackedInline):
+    model = Chapters
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    inlines = [ChaptersInline]
     list_display = ['series_title', 'author']
     search_fields = ['series_title', 'alternative']
 
 
-admin.site.register(Series, SeriesModelAdmin)
-admin.site.register(Chapters)
+admin.site.register(Series, SeriesAdmin)
 admin.site.register(Ratings)
